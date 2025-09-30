9e1c859836e779c3dd304096a5042e9737f83b24
Henrique Galinha <hgalinha1999@hotmail.com>
2025-09-30 14:11:40 +0100

    header filter update

--- super-nova-2177/frontend/content/filters/FilterHeader.jsx
+++ super-nova-2177/frontend/content/filters/FilterHeader.jsx
@@ -3,7 +3,7 @@ import Filters from "./Filters"
 
 function FilterHeader({filter, setFilter, setSearch, search}) {
         return (
-        <div className="fixed top-1 lg:top-50 lg:right-10 lg:w-10 xl:top-50 xl:left-1/2 xl:translate-x-[415px] max-w-65 md:max-w-130 min-w-100 lg:min-w-54 bg-white shadow-md p-2 rounded-[25px] flex flex-row lg:flex-col gap-2 lg:h-26 w-full justify-end lg:justify-between mb-[-20px] lg:mb-0 ">
+        <div className="z-90 fixed top-1 lg:top-50 lg:right-10 lg:w-10 xl:top-50 xl:left-1/2 xl:translate-x-[415px] max-w-65 md:max-w-130 min-w-100 lg:min-w-54 bg-white shadow-md p-2 rounded-[25px] flex flex-row lg:flex-col gap-2 lg:h-26 w-full justify-end lg:justify-between mb-[-20px] lg:mb-0 ">
             <Filters filter={filter} setFilter={setFilter} />
             <Input setSearch={setSearch} search={search}/>
         </div>
