54942fad8112bec79b9059098117ff399cd9be07
Henrique Galinha <hgalinha1999@hotmail.com>
2025-10-10 09:28:58 +0100

    Revert "update scroll"
    This reverts commit 1cd01d57cb40074fca78248682577b9ab7d17b2f.

--- super-nova-2177/frontend/content/create post/CreatePost.jsx
+++ super-nova-2177/frontend/content/create post/CreatePost.jsx
@@ -1,31 +1,7 @@
 import { FaPlus } from "react-icons/fa6";
 import LiquidGlass from "../liquid glass/LiquidGlass";
-import { useState, useEffect } from "react";
 
 function CreatePost({ setDiscard }) {
-  const [showButton, setShowButton] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.innerWidth > 1024) {
-        setShowButton(true);
-        return;
-      }
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > lastScrollY && currentScrollY > 50) {
-        setShowButton(false);
-      } else {
-        setShowButton(true);
-      }
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
-
   const handleClick = () => {
     setDiscard(false);
     const element = document.getElementById("createPost");
@@ -37,11 +13,7 @@ function CreatePost({ setDiscard }) {
   return (
     <button
       onClick={handleClick}
-      className={`z-9001 w-fit fixed left-1/2 bottom-5.5 -translate-x-1/2 lg:top-30 lg:bottom-auto flex justify-center items-center group transition-transform duration-300 ${
-        showButton
-          ? "translate-y-0 opacity-100"
-          : "translate-y-20 opacity-0 pointer-events-none"
-      }`}
+      className="z-9001 w-fit fixed left-1/2 bottom-5.5 -translate-x-1/2 lg:top-30 lg:bottom-auto flex justify-center items-center group"
     >
       <LiquidGlass className="h-12.5 w-12.5 lg:w-fit lg:h-fit cursor-pointer hover:scale-98 rounded-[20px] lg:rounded-full p-0 lg:p-1 flex items-center">
         <LiquidGlass className="text-black opacity-70 rounded-full flex items-center justify-center">
